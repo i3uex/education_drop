7a508fb16819819f03ba997844c2a89a018e11bd
Fran <frmelchor@alumnos.unex.es>
2021-07-07 13:51:52 +0200

    Allow to indicate school_kind by arguments

--- Code/Data_Acquisition_and_Understanding/ETL/etl_record_personal_access/etl_record_personal_access.py
+++ Code/Data_Acquisition_and_Understanding/ETL/etl_record_personal_access/etl_record_personal_access.py
@@ -2,11 +2,14 @@ import logging
 from apitep_utils import ETL
 from apitep_utils.transformation import Transformation
 import keys
-from Code.data_model.titulation_kind import SchoolKind
+from typing import List
+from Code.data_model.school_kind import SchoolKind
 import pandas as pd
+from apitep_utils import ArgumentParserHelper
+import argparse
 
 log = logging.getLogger(__name__)
-pr_plan_subj_call: pd.DataFrame = ""
+pr_plan_subj_call: pd.DataFrame
 
 
 def delete_people_without_all_information(p):
@@ -25,33 +28,46 @@ def delete_people_without_all_information(p):
 class RecordPersonalAccessETL(ETL):
     school_kind: SchoolKind = None
 
-    def __init__(
-            self,
-            input_path_segment: str = None,
-            output_path_segment: str = None,
-            input_separator: str = None,
-            output_separator: str = None,
-            save_report_on_load: bool = None,
-            save_report_on_save: bool = None,
-            report_type: Transformation.ReportType = None,
-            report_path_segment: str = None,
-            input_type_excel: bool = None,
-            school_kind: SchoolKind = None,
-    ):
-        super().__init__(
-            input_path_segment=input_path_segment,
-            output_path_segment=output_path_segment,
-            input_separator=input_separator,
-            output_separator=output_separator,
-            save_report_on_load=save_report_on_load,
-            save_report_on_save=save_report_on_save,
-            report_type=report_type,
-            report_path_segment=report_path_segment,
-            input_type_excel=input_type_excel
-        )
+    def parse_arguments(self):
+        """
+        Parse arguments provided via command line, and check if they are valid
+        or not. Adequate defaults are provided when possible.
 
-        if school_kind is not None:
-            self.school_kind = school_kind
+        Parsed arguments are:
+        - paths to the input CSV datasets, separated with spaces.
+        - path to the output CSV dataset.
+        """
+
+        log.info("Get integration arguments")
+        log.debug("Integration.parse_arguments()")
+
+        program_description = self.description
+        argument_parser = argparse.ArgumentParser(description=program_description)
+        argument_parser.add_argument("-i", "--input_paths",
+                                     required=True,
+                                     nargs="+",
+                                     help="path to the input CSV datasets")
+        argument_parser.add_argument("-o", "--output_path", required=True,
+                                     help="path to the output CSV dataset")
+        argument_parser.add_argument("-s", "--school_kind", required=True,
+                                     help="school kind to analyze")
+
+        arguments = argument_parser.parse_args()
+        input_path_segments = arguments.input_paths
+        self.input_path_segments = []
+        for input_path_segment in input_path_segments:
+            self.input_path_segments.append(
+                ArgumentParserHelper.parse_data_file_path(
+                    data_file_path=input_path_segment)
+            )
+        self.output_path_segment = ArgumentParserHelper.parse_data_file_path(
+            data_file_path=arguments.output_path,
+            check_is_file=False)
+        school_kind_str = arguments.school_kind
+        if school_kind_str == "Teaching":
+            self.school_kind = SchoolKind.Teaching
+        elif school_kind_str == "Polytechnic":
+            self.school_kind = SchoolKind.Polytechnic
 
     @ETL.stopwatch
     def process(self):
@@ -59,74 +75,61 @@ class RecordPersonalAccessETL(ETL):
         Process record_personal_access data
         """
 
-        log.info("Process record_personal_access data")
+        log.info("Process record_personal_access data of school: " + self.school_kind.value)
         log.debug("RecordPersonalAccessETL.process()")
 
-        global pr_plan_subj_call, record_personal_access
+        global pr_plan_subj_call
 
         if self.school_kind is SchoolKind.Polytechnic:
             old_degrees = ['MÁSTER UNIVERSITARIO EN COMPUTACIÓN GRID Y PARALELISMO',
                            'MÁSTER DE ESPECIALIZACIÓN EN GEOTECNOLOGÍAS TOPOGRÁFICAS EN LA INGENIERÍA',
                            'MÁSTER UNIVERSITARIO EN EVALUACIÓN Y GESTIÓN DEL RUIDO AMBIENTAL',
                            'MÁSTER EN COMPUTACIÓN GRID Y PARALELISMO']
-            pr_plan_subj_call = pd.read_csv('../../../../Data/Processed/Polytechnic/Plan_Subject_Call'
-                                            '/pr_polytechnic_plan_subject_call.csv', sep='|')
         elif self.school_kind is SchoolKind.Teaching:
             old_degrees = ['MÁSTER UNIVERSITARIO EN INVESTIGACIÓN EN CIENCIAS SOCIALES Y JURÍDICAS',
                            'MÁSTER UNIV. FORMACIÓN EN PORTUGUÉS PARA PROF. ENSEÑANZA PRIM. Y SECUNDARIA']
-            pr_plan_subj_call = pd.read_csv('../../../../Data/Processed/Teaching/Plan_Subject_Call'
-                                            '/pr_teaching_plan_subject_call.csv', sep='|')
         else:
             raise NotImplementedError
 
         no_valid_courses = ['2017-18', '2018-19', '2019-20', '2020-21']
+        pr_plan_subj_call = self.input_dfs[1]
 
-        log.info("initial number of rows: " + str(len(self.input_df.index)))
-        rows_before = len(self.input_df.index)
-        self.input_df = self.input_df[self.input_df[keys.PLAN_DESCRIPTION_KEY].isin(old_degrees) == False]
-        rows_after = len(self.input_df.index)
+        log.info("initial number of rows: " + str(len(self.input_dfs[0].index)))
+        rows_before = len(self.input_dfs[0].index)
+        self.input_dfs[0] = self.input_dfs[0][self.input_dfs[0][keys.PLAN_DESCRIPTION_KEY].isin(old_degrees) == False]
+        rows_after = len(self.input_dfs[0].index)
         self.changes["delete data of old degrees"] = rows_before - rows_after
 
-        rows_before = len(self.input_df.index)
-        df_filter = self.input_df.apply(delete_people_without_all_information, axis=1)
-        self.input_df = self.input_df[df_filter]
-        self.input_df = self.input_df[self.input_df[keys.OPEN_YEAR_PLAN_KEY].isin(no_valid_courses) == False]
-        rows_after = len(self.input_df.index)
+        rows_before = len(self.input_dfs[0].index)
+        df_filter = self.input_dfs[0].apply(delete_people_without_all_information, axis=1)
+        self.input_dfs[0] = self.input_dfs[0][df_filter]
+        self.input_dfs[0] = self.input_dfs[0][self.input_dfs[0][keys.OPEN_YEAR_PLAN_KEY].isin(no_valid_courses) == False]
+        rows_after = len(self.input_dfs[0].index)
         self.changes["delete data of people without all information"] = rows_before - rows_after
 
-        self.input_df[keys.BIRTH_DATE_KEY] = pd.to_datetime(self.input_df[keys.BIRTH_DATE_KEY])
-        self.input_df[keys.BIRTH_DATE_KEY] = self.input_df[keys.BIRTH_DATE_KEY].apply(lambda func: func.year)
-        self.input_df.rename(columns={keys.BIRTH_DATE_KEY: keys.BIRTH_YEAR_KEY}, inplace=True)
+        self.input_dfs[0][keys.BIRTH_DATE_KEY] = pd.to_datetime(self.input_dfs[0][keys.BIRTH_DATE_KEY])
+        self.input_dfs[0][keys.BIRTH_DATE_KEY] = self.input_dfs[0][keys.BIRTH_DATE_KEY].apply(lambda func: func.year)
+        self.input_dfs[0].rename(columns={keys.BIRTH_DATE_KEY: keys.BIRTH_YEAR_KEY}, inplace=True)
         self.changes["get only year of birth date"] = rows_after
 
-        rows_affected = len(self.input_df[pd.isna(self.input_df[keys.TRANSFER_TYPE_KEY])].index)
-        self.input_df[keys.TRANSFER_TYPE_KEY] = self.input_df[keys.TRANSFER_TYPE_KEY].apply(
+        rows_affected = len(self.input_dfs[0][pd.isna(self.input_dfs[0][keys.TRANSFER_TYPE_KEY])].index)
+        self.input_dfs[0][keys.TRANSFER_TYPE_KEY] = self.input_dfs[0][keys.TRANSFER_TYPE_KEY].apply(
             lambda func: 'N' if pd.isna(func) else func)
         self.changes["resolve values null of tipo_tralado column"] = rows_affected
 
-        log.info("columns of final dataset are:" + self.input_df.columns)
-        log.info("final number of rows: " + str(len(self.input_df.index)))
+        log.info("columns of final dataset are:" + self.input_dfs[0].columns)
+        log.info("final number of rows: " + str(len(self.input_dfs[0].index)))
 
-        self.output_df = self.input_df
+        self.output_df = self.input_dfs[0]
 
 
 def main():
-    school_kind = SchoolKind.Teaching
 
-    if school_kind is SchoolKind.Polytechnic:
-        logging.basicConfig(
-            filename="pol_debug.log",
-            level=logging.DEBUG,
-            format="%(asctime)-15s %(levelname)8s %(name)s %(message)s")
-        logging.getLogger("matplotlib").setLevel(logging.ERROR)
-    elif school_kind is school_kind.Teaching:
-        logging.basicConfig(
-            filename="teach_debug.log",
-            level=logging.DEBUG,
-            format="%(asctime)-15s %(levelname)8s %(name)s %(message)s")
-        logging.getLogger("matplotlib").setLevel(logging.ERROR)
-    else:
-        raise NotImplementedError
+    logging.basicConfig(
+        filename="debug.log",
+        level=logging.DEBUG,
+        format="%(asctime)-15s %(levelname)8s %(name)s %(message)s")
+    logging.getLogger("matplotlib").setLevel(logging.ERROR)
 
     log.info("Start RecordPersonalAccessETL")
     log.debug("main()")
@@ -134,8 +137,9 @@ def main():
     etl = RecordPersonalAccessETL(
         input_separator="|",
         output_separator="|",
+        save_report_on_save=False,
+        save_report_on_load=False,
         report_type=ETL.ReportType.Both,
-        school_kind=school_kind
     )
     etl.execute()
 
