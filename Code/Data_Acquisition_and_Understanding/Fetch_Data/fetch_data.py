fa61d731a9b95e12926436586670b5f5ab612920
Fran <frmelchor@alumnos.unex.es>
2021-07-16 12:47:26 +0200

    "Add logs messages"

--- Code/Data_Acquisition_and_Understanding/Fetch_Data/fetch_data.py
+++ Code/Data_Acquisition_and_Understanding/Fetch_Data/fetch_data.py
@@ -11,13 +11,16 @@ class FetchData(ETL):
         """
         Process raw data of education drop
         """
-        columns = list(self.input_df.columns)
+        columns = list(self.input_dfs[0].columns)
         columns.remove("expediente")
         columns.insert(0, "expediente")
 
         log.info("reorder columns of dataset and set on top column 'expediente'")
 
-        self.output_df = self.input_df[columns]
+        self.output_df = self.input_dfs[0][columns]
+
+        log.info("columns of dataset are:" + str(self.input_dfs[0].columns))
+        log.info("final number of rows: " + str(len(self.input_dfs[0].index)))
 
 
 def main():
@@ -27,6 +30,7 @@ def main():
         format="%(asctime)-15s %(levelname)8s %(name)s %(message)s")
     logging.getLogger("matplotlib").setLevel(logging.ERROR)
 
+    log.info("-------------------------------------------------------")
     log.info("Start FetchData")
     log.debug("main()")
 
