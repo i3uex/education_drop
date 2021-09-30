d5e8a664db04df77e30609b4045539afa187204f
Fran <frmelchor@alumnos.unex.es>
2021-09-30 16:32:59 +0200

    Refactor code

--- run_project.sh
+++ run_project.sh
@@ -1,7 +1,8 @@
+sudo apt install git
+pip install -r requirements.txt
 dvc remote modify storage --local gdrive_service_account_json_file_path "$(pwd)"/education-drop-d884963a3b3b.json
 dvc fetch
 dvc pull
-pip install -r requirements.txt
 PYTHONPATH="$(pwd)"/Code
 export PYTHONPATH
 python init.py -rd "$(pwd)"
