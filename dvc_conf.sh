3e3beaf34d84519be10c70c88a1d15e04e1abef2
Fran <frmelchor@alumnos.unex.es>
2021-11-23 11:16:33 +0100

    Init validation KNIME

---
+++ dvc_conf.sh
@@ -0,0 +1,2 @@
+dvc remote modify storage --local gdrive_service_account_json_file_path "$(pwd)"/education-drop-d884963a3b3b.json
+dvc pull
--- run_project.sh
+++
@@ -1,6 +0,0 @@
-dvc remote modify storage --local gdrive_service_account_json_file_path "$(pwd)"/education-drop-d884963a3b3b.json
-dvc pull
-PYTHONPATH="$(pwd)"/Code
-export PYTHONPATH
-python init.py -rd "$(pwd)"
-dvc repro
